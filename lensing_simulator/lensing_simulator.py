a11510fdd0f1142d7be993ee14214c8b02fd09ee
Jasmine Boyd <jasminevb5017@gmail.com>
2023-11-04 11:22:33 -0400

    Bf is mean (add output image size)

--- lensing_simulator/lensing_simulator.py
+++ lensing_simulator/lensing_simulator.py
@@ -13,8 +13,11 @@ def main():
     parser.add_argument('camera_to_mass', type = float, help = "The Distance from the Observer to the Mass")
     parser.add_argument('mass_to_object', type = float, help = "Distance from the Lensing Mass to the Lensed Image")
     parser.add_argument('mass_radius', type = float, help = "The Radius of the Lensing Mass")
+    parser.add_argument('-x', '--output_image_size_x', type = int, help = "The X Co-ordinate of the Outputted Image", default = 1920)
+    parser.add_argument('-y', '--output_image_size_y', type = int, help = "The Y Co-ordinate of the Outputted Image", default = 1080)
+
     args = parser.parse_args()
     image = Image.open(args.image_file.name)
-    image.show()
+    
 
 main()
