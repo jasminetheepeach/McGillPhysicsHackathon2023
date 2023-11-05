92d720ac6da26606b7126c968b4170827ab39ce3
Akash Shah <akash.w.shah@gmail.com>
2023-11-04 22:42:11 -0400

    Fix spelling of near clipping plane

--- lensing_simulator/lensing_simulator.py
+++ lensing_simulator/lensing_simulator.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('-t', '--time_step', type = float, help = "The time step. Lower value results in a more accurate simulation but longer time taken.", default = 0.01)
     parser.add_argument('-l', '--time_step_limit', type = float, help = "The limit of the number of time steps allowed to occur", default = 1000)
     parser.add_argument('-f', '--field_of_view', type = float, help = "The horizontal angle viewed", default = 135)
-    parser.add_argument('-p', '--near_clipping_place_distance', type = float, help = "The distance to the near clipping place", default = 1)
+    parser.add_argument('-p', '--near_clipping_plane_distance', type = float, help = "The distance to the near clipping plane", default = 1)
 
     args = parser.parse_args()
 
